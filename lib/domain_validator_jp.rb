a16d1a058a94e293eb24e5e351a88f3e7b532789
kimromi <kimromi4@gmail.com>
2018-06-15 16:49:41 +0900

    Add PublicSuffix check

--- lib/domain_validator_jp.rb
+++ lib/domain_validator_jp.rb
@@ -1,9 +1,15 @@
 require "domain_validator_jp/version"
+require "public_suffix"
 
 module DomainValidatorJp
   class << self
     def valid?(domain)
+      return false unless valid_public_suffix?(domain)
       true
+    end
+
+    def valid_public_suffix?(domain)
+      PublicSuffix.valid?(domain, default_rule: nil, ignore_private: true)
     end
   end
 end
